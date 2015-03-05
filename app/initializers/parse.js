daf1e0b746521fd7c6d7193663352f752aee0fbb
McLeanBH <mcleanbh@gmail.com>
2015-03-05 18:35:49 -0500

    configured with trey's parse account, and rendering on page

--- app/initializers/parse.js
+++ app/initializers/parse.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export function initialize(/* container, application */) {
-  Ember.$.ajaxSetup ({
-     headers: {
-       "X-Parse-Application-Id": 'weA6sJ27JjsBF03ztfsWUvjFrLe5ITawULO54CuP',
-       "X-Parse-REST-API-Key": 'MLrNgtMbLXTSnAonaFqFtkkmSxlfKPiLu9sjjSgv'
-     }
+ Ember.$.ajaxSetup ({
+    headers: {
+      "X-Parse-Application-Id": 'um2UFrNxo3qc1YfibgAueaeQy2APInooospgdqr3',
+      "X-Parse-REST-API-Key": 'ViNtz0I8lxQ6YVHJnOVTvomGoezpAObxqqNHaMwD'
+    }
 });
 }
 
 export default {
-  name: 'parse',
-  initialize: initialize
+ name: 'parse',
+ initialize: initialize
 };
--- app/routes/recipes/index.js
+++ app/routes/recipes/index.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function(){
-    return Ember.$.ajax("https://api.parse.com/1/classes/recipe").then(function(response) {
-      console.log(response);
-      return response.results;
+ model: function(){
+   return Ember.$.ajax("https://api.parse.com/1/classes/Recipe").then(function(response) {
+     console.log(response);
+     return response.results;
 
-    });
-  }
+   });
+ }
 });
