f24ecb8b294eb08ff146242e7448d5b1a11ff08c
Trey Phillips <tlphillipsjr@gmail.com>
2015-03-05 17:57:40 -0500

    updated index template

--- app/initializers/parse.js
+++ app/initializers/parse.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function initialize(/* container, application */) {
   Ember.$.ajaxSetup ({
      headers: {
-       "X-Parse-Application-Id": 'weA6sJ27JjsBF03ztfsWUvjFrLe5ITawULO54CuP',
-       "X-Parse-REST-API-Key": 'MLrNgtMbLXTSnAonaFqFtkkmSxlfKPiLu9sjjSgv'
+       "X-Parse-Application-Id": 'um2UFrNxo3qc1YfibgAueaeQy2APInooospgdqr3',
+       "X-Parse-REST-API-Key": 'ViNtz0I8lxQ6YVHJnOVTvomGoezpAObxqqNHaMwD'
      }
 });
 }
